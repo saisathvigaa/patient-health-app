616239f197de8b3c3e6f8c878ef4aa47e9cfada5
saisathvigaa <52745477+saisathvigaa@users.noreply.github.com>
2026-05-24 07:08:24 -0400

    Integrate Prisma with accelerate extension

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
-      globalForPrisma.prisma ||
-      new PrismaClient({
-              log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-      });
+  globalForPrisma.prisma ||
+  (new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
