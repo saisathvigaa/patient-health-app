e8e9adf528f969b567aa6bfdc0283ca2e91c521b
saisathvigaa <52745477+saisathvigaa@users.noreply.github.com>
2026-05-24 06:31:45 -0400

    Refactor PrismaClient creation to remove datasourceUrl
    Removed datasourceUrl from PrismaClient instantiation.

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -4,10 +4,8 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore - datasourceUrl is valid in Prisma 7 runtime but not yet in TS types
-  return new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
-    .$extends(withAccelerate()) as unknown as PrismaClient;
+    return new PrismaClient()
+      .$extends(withAccelerate()) as unknown as PrismaClient;
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
